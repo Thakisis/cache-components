ae3a9c844081c77042d2749a5f16e640e5e81090
Thakisis <sub722@gmail.com>
2026-03-17 19:34:11 +0100

    update: Next config default cache

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  
+
   cacheComponents: true,
+  cacheLife: {
+    default: {
+      stale: Number.MAX_SAFE_INTEGER, // máximo posible
+      revalidate: Number.MAX_SAFE_INTEGER,
+      // expire omitido = nunca expira (Infinity)
+    },
+  },
   images: {
     formats: ["image/avif", "image/webp"],
   },
