a93298f4990db670c76e2457a8072d57074f32b0
Andrei <andreiflorianbratila@gmail.com>
2026-03-18 10:26:57 +0100

    solo array

--- src/components/home/product-list/product-list.tsx
+++ src/components/home/product-list/product-list.tsx
@@ -1,12 +1,13 @@
 
-import { getProductIds } from "@/server/queries/getProductIds";
+// import { getProductIds } from "@/server/queries/getProductIds";
 import ProductCard from "./product-card";
+const arrayProductsIds = Array.from({ length: 10 }, (_, i) => i + 1);
 export default async function ProductList() {
-  const productosIds = await getProductIds(10);
-  
+  // const productosIds = await getProductIds(10);
+
   return (
-  productosIds.map((product) => (
-    <ProductCard key={product.id} id={product.id} />
+  arrayProductsIds.map((product) => (
+    <ProductCard key={product} id={product} />
   ))
   );
 }
